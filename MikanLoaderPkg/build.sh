6d11428c6bb768277513ecfe89f16464ef00bd19
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2025-02-18 12:37:17 +0000

    save memory map

--- MikanLoaderPkg/build.sh
+++ MikanLoaderPkg/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-(cd /home/ubuntu/edk2 && . ./edksetup.sh && build)
+(cd /home/ubuntu/edk2 && . ./edksetup.sh && build) || exit 1
 ${HOME}/dev/osbook/devenv/make_image.sh ./disk.img ./mnt Loader.efi

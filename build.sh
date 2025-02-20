12a21ac0cddb2035ac2049b0b51c18b6163112f3
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2025-02-20 16:06:27 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ clang++ -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-excep
 ld.lld --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o \
 )
 
-DISK_IMG="$HOME/dev/mikanos/mikanos.img"
+DISK_IMG="$HOME/mikanos/mikanos.img"
 MOUNT_POINT=./mnt
 EFI_FILE="$HOME/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi"
-KERNEL_FILE="$HOME/dev/mikanos/kernel/kernel.elf"
+KERNEL_FILE="$HOME/mikanos/kernel/kernel.elf"
 
-(cd /home/ubuntu/edk2 && . ./edksetup.sh && build)
-${HOME}/dev/osbook/devenv/make_image.sh "$DISK_IMG" "$MOUNT_POINT" "$EFI_FILE" "$KERNEL_FILE"
+(cd $HOME/edk2 && . ./edksetup.sh && build)
+${HOME}/osbook/devenv/make_image.sh "$DISK_IMG" "$MOUNT_POINT" "$EFI_FILE" "$KERNEL_FILE"

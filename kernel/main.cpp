70f5acaf6c37141490bdd2645801cd1f75e09215
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2025-02-23 13:30:56 +0900

    fix for loop

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -38,13 +38,13 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
     for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
       WritePixel(frame_buffer_config, x, y, {255, 255, 255});
     }
-
-    for (int x = 0; x < 200; ++x) {
-      for (int y = 0; y < 100; ++y) {
-        WritePixel(frame_buffer_config, 100 + x, 100 + y, {0, 255, 0});
-      }
-    }
-
-    Halt();
   }
+
+  for (int x = 0; x < 200; ++x) {
+    for (int y = 0; y < 100; ++y) {
+      WritePixel(frame_buffer_config, 100 + x, 100 + y, {0, 255, 0});
+    }
+  }
+
+  Halt();
 }

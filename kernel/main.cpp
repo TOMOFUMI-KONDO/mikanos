371f8f6118844ac3311dad3865c6ec217b860852
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2025-02-23 09:26:42 +0000

    replace pixel-writer buffer outside of main func

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -14,22 +14,20 @@ void Halt(void) {
     __asm__("hlt");
 }
 
-extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
-  PixelWriter *pixel_writer;
+char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+PixelWriter *pixel_writer;
 
+extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
-  case kPixelRGBResv8BitPerColor: {
-    char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+  case kPixelRGBResv8BitPerColor:
     pixel_writer = new (pixel_writer_buf)
         RGBResv8BitPerColorPixelWriter{frame_buffer_config};
     break;
-  }
-  case kPixelBGRResv8BitPerColor: {
+  case kPixelBGRResv8BitPerColor:
     char pixel_writer_buf[sizeof(BGRResv8BitPerColorPixelWriter)];
     pixel_writer = new (pixel_writer_buf)
         BGRResv8BitPerColorPixelWriter{frame_buffer_config};
     break;
-  }
   default:
     Halt();
   }
